da27ecb82997f288f7042d6ba1f2d01e66f1f6f1
Shadowmaple <1142319190@qq.com>
2021-05-07 11:01:02 +0800

    :rocket: add public cros admission

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,12 @@
 from config import Config
 from flask import Flask
+from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
+CORS(app, supports_credentials=True)
 
 from .api import api as api_app
 
